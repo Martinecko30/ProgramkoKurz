2995ce6102f7714825777450e321e0de33cf3c91
Martinecko30 <52097833+Martinecko30@users.noreply.github.com>
2025-09-01 23:05:29 +0200

    Added score

--- Assets/Scripts/Bird.cs
+++ Assets/Scripts/Bird.cs
@@ -1,15 +1,21 @@
 using System;
+using TMPro;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class Bird : MonoBehaviour
 {
+    // [SerializeField] allows private properties to be modified in editor
+    
     private Rigidbody rigidbody;
     [SerializeField] private float jumpForce = 10, speed = 5;
     [SerializeField] private Transform floor, camera;
     private Vector3 floorOffset, cameraOffset;
     
     [SerializeField] private PillairSpawner pillairSpawner;
+
+    [SerializeField] private TMP_Text scoreText;
+    private int score = 0;
     
     private bool collided = false;
     
@@ -19,33 +25,40 @@ public class Bird : MonoBehaviour
         
         floorOffset = floor.position;
         cameraOffset = camera.position;
+        
+        scoreText.text = score.ToString();
     }
     
     void Update()
     {
+        // Drops our bird down, we are dead
         if (collided)
             return;
         
+        // Adds forwards motion
         rigidbody.linearVelocity = new Vector3(speed, rigidbody.linearVelocity.y, 0);
         
         floor.position = new Vector3(transform.position.x + floorOffset.x, floorOffset.y, transform.position.z + floorOffset.z);
         camera.position = new Vector3(transform.position.x + cameraOffset.x, cameraOffset.y, transform.position.z + cameraOffset.z);
 
+        // Jumping
         var keyboard = Keyboard.current;
-
         if (keyboard.spaceKey.wasPressedThisFrame)
-        {
             rigidbody.linearVelocity = new Vector3(rigidbody.linearVelocity.x, jumpForce, 0);
-        }
     }
 
+    // We collided, therefore we are dead
     private void OnCollisionEnter(Collision other)
     {
         collided = true;
     }
 
+    // Spawn new Pillair and modify score
     private void OnTriggerEnter(Collider other)
     {
         pillairSpawner.SpawnPillair(transform.position);
+        
+        score++;
+        scoreText.text = score.ToString();
     }
 }
--- Assets/Scripts/PillairSpawner.cs
+++ Assets/Scripts/PillairSpawner.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class PillairSpawner : MonoBehaviour
 {
@@ -15,14 +16,22 @@ public class PillairSpawner : MonoBehaviour
 
     public void SpawnPillair(Vector3 position)
     {
-        if (pillairs.Count >= 2)
+        // Removes the first spawned pillair
+        // Max pillairs defines how many it can spawn before deleting first one
+        int maxPillairs = 4;
+        if (pillairs.Count >= maxPillairs)
         {
             var leftPillair = pillairs[0];
             Destroy(leftPillair);
             pillairs.RemoveAt(0);
         }
         
-        var newPillair = Instantiate(pillair, position + offset, Quaternion.identity);
+        // Random offset, moves up and down
+        //var randomOffset = new Vector3(0, Random.Range(-0.5f, 0.5f), 0);
+        var randomOffset = Vector3.up * Random.Range(-0.5f, 0.5f); // Preferred way
+        
+        // Instantiates new one and adds it to list
+        var newPillair = Instantiate(pillair, position + offset + randomOffset, Quaternion.identity);
         pillairs.Add(newPillair);
     }
 }
